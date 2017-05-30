8ac3e34bdf41122ed89bbc13e444902d4101cbe0
Pinguet62 <pinguet62@gmail.com>
2017-05-31 00:42:23 +0200

    Fix i18n

--- javadoc/src/test/java/fr/pinguet62/xjc/javadoc/option/StrategyTest.java
+++ javadoc/src/test/java/fr/pinguet62/xjc/javadoc/option/StrategyTest.java
@@ -4,6 +4,7 @@ import static fr.pinguet62.xjc.common.test.JavaParserUtils.formatComments;
 import static fr.pinguet62.xjc.javadoc.JavadocPluginTestRunner.runDriverAndParseClass;
 import static fr.pinguet62.xjc.javadoc.option.Strategy.APPEND_BEGIN;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -24,10 +25,9 @@ public class StrategyTest {
         List<String> comments = formatComments(type.getComment());
         assertEquals("Comment of xs:element CommentedClass", comments.get(0));
         assertEquals("", comments.get(1));
-        assertEquals("<p>Classe Java pour CommentedClass complex type.", comments.get(2));
+        assertTrue(comments.get(2).matches("<p>.*CommentedClass.*"));
         assertEquals("", comments.get(3));
-        assertEquals("<p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.",
-                comments.get(4));
+        assertTrue(comments.get(4).matches("<p>.*"));
     }
 
     @Test
